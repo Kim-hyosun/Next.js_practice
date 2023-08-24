90f231d22dd906b40546482d92045ec8bd756629
Kim-hyosun <jirehmuze@gmail.com>
2023-08-24 22:37:56 +0900

    api/hello api만들기공부

---
+++ models/firebase_admin.ts
@@ -0,0 +1,58 @@
+import * as admin from 'firebase-admin';
+
+interface Config {
+  credential: {
+    privateKey: string;
+    clientEmail: string;
+    projectId: string;
+  };
+}
+
+export default class FirebaseAdmin {
+  public static instance: FirebaseAdmin;
+
+  private init = false;
+
+  public static getInstanse(): FirebaseAdmin {
+    if (FirebaseAdmin.instance === undefined || FirebaseAdmin.instance === null) {
+      //초기화 진행
+      FirebaseAdmin.instance = new FirebaseAdmin();
+      //TODO: 환경을 초기화 한다.
+      FirebaseAdmin.instance.bootstrap();
+    }
+    return FirebaseAdmin.instance;
+  }
+
+  //환경을 초기화할 때 사용할 메서드
+  private bootstrap(): void {
+    const haveApp = admin.apps.length !== 0; //등록된 앱이 존재하면?
+    if (haveApp) {
+      this.init = true;
+      return;
+    }
+    const config: Config = {
+      credential: {
+        projectId: process.env.projectId || '',
+        clientEmail: process.env.clientEmail || '',
+        privateKey: (process.env.privateKey || '').replace(/\\n/g, '\n'),
+      },
+    };
+    admin.initializeApp({ credential: admin.credential.cert(config.credential) });
+    console.info('bootstrap firebase admin');
+  }
+
+  /** firestore를 반환 */
+  public get Firebase(): FirebaseFirestore.Firestore {
+    if (this.init === false) {
+      this.bootstrap();
+    }
+    return admin.firestore();
+  }
+
+  public get Auth(): admin.auth.Auth {
+    if (this.init === false) {
+      this.bootstrap();
+    }
+    return admin.auth();
+  }
+}
