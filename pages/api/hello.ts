fb32e9fef39a5177b32d248d237b96c985c3f0cd
Kim-hyosun <jirehmuze@gmail.com>
2023-09-30 02:46:13 +0900

    사용자 페이지뷰, message입력 컴포넌트, message서버로 넘기는 api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -4,6 +4,6 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import FirebaseAdmin from '@/models/firebase_admin';
 
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
-  FirebaseAdmin.getInstanse().Firebase.collection('test');
+  FirebaseAdmin.getInstanse().Firestore.collection('test');
   res.status(200).json({ name: 'John Doe' });
 }
