922ccc54e20b81c6b715f87aada01f7cf4a99a10
Kim-hyosun <jirehmuze@gmail.com>
2023-09-29 20:57:45 +0900

    user model추가

--- pages/api/members.add.ts
+++ pages/api/members.add.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import FirebaseAdmin from '@/models/firebase_admin';
+
+import MemberModel from '@/models/member/member.model';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { uid, email, displayName, photoURL } = req.body;
@@ -10,36 +11,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if (email === undefined || email === null) {
     return res.status(400).json({ result: false, message: 'email이 누락되었습니다.' });
   }
-  try {
-    const screenName = (email as string).replace('@gmail.com', '');
-
-    const addResult = await FirebaseAdmin.getInstanse().Firebase.runTransaction(async (transaction) => {
-      const memberRef = FirebaseAdmin.getInstanse().Firebase.collection('members').doc(uid);
-
-      const screenNameRef = FirebaseAdmin.getInstanse().Firebase.collection('screen_names').doc(screenName);
-
-      const memberDoc = await transaction.get(memberRef);
-
-      if (memberDoc.exists) {
-        //이미 추가된 상태
-        return false;
-      }
-      const addData = {
-        uid,
-        email,
-        displayName: displayName ?? '',
-        photoURL: photoURL ?? '',
-      };
-      transaction.set(memberRef, addData);
-      transaction.set(screenNameRef, addData);
-      return true;
-    });
-    if (addResult === false) {
-      return res.status(201).json({ result: true, id: uid });
-    }
-    return res.status(200).json({ result: true, id: uid });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ result: false });
+  const addResult = await MemberModel.add({ uid, email, displayName, photoURL });
+  if (addResult.result === true) {
+    return res.status(200).json(addResult);
   }
+  res.status(500).json(addResult);
 }
